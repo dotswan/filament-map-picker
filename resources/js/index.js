4a5f8e710bf899e54e2520be39ead4dc933c1f70
Hessam Taghvaei <mohaphez@yahoo.com>
2024-12-29 23:32:55 +0330

    feat: add markerHtml, markerIconUrl , markerIconSize , markerIconClassName, markerIconAnchor methods

--- resources/js/index.js
+++ resources/js/index.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
             drawItems: null,
             rangeSelectField: null,
             formRestorationHiddenInput:null,
-
+            
             createMap: function (el) {
                 const that = this;
 
@@ -59,21 +59,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 }).addTo(this.map);
 
                 if (config.showMarker) {
-                    const markerColor = config.markerColor || "#3b82f6";
-                    const svgIcon = LF.divIcon({
-                        html: `<svg xmlns="http://www.w3.org/2000/svg" class="map-icon" fill="${markerColor}" width="36" height="36" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg>`,
-                        className: "",
-                        iconSize: [36, 36],
-                        iconAnchor: [18, 36],
-                    });
                     this.marker = LF.marker(this.getCoordinates(), {
-                        icon: svgIcon,
+                        icon: this.createMarkerIcon(),
                         draggable: false,
                         autoPan: true
                     }).addTo(this.map);
                     this.setMarkerRange();
-                    if(!config.clickable)
-                    {
+                    if(!config.clickable) {
                         this.map.on('move', () => this.setCoordinates(this.map.getCenter()));
                     }
                 }
@@ -213,6 +205,26 @@ document.addEventListener('DOMContentLoaded', () => {
                             this.map.fitBounds(this.drawItems.getBounds());
                     }
               }
+            },
+            createMarkerIcon() {
+                if (config.markerIconUrl) {
+                    return LF.icon({
+                        iconUrl: config.markerIconUrl,
+                        iconSize: config.markerIconSize,
+                        iconAnchor: config.markerIconAnchor,
+                        className: config.markerIconClassName
+                    });
+                }
+
+                const markerColor = config.markerColor || "#3b82f6";
+                const defaultHtml = `<svg xmlns="http://www.w3.org/2000/svg" class="map-icon" fill="${markerColor}" width="36" height="36" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg>`;
+                
+                return LF.divIcon({
+                    html: config.markerHtml || defaultHtml,
+                    className: config.markerIconClassName,
+                    iconSize: config.markerIconSize,
+                    iconAnchor: config.markerIconAnchor
+                });
             },
             initFormRestoration: function () {
                 this.formRestorationHiddenInput = document.getElementById(config.statePath+'_fmrest');
