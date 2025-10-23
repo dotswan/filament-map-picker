7be0293169139339df30b1dc985fc711ae576922
Hessam Taghvaei <mohaphez@yahoo.com>
2025-10-23 13:02:05 +0330

    Enhance form restoration handling in map picker component
    - Updated form restoration input retrieval to use refs for better compatibility.
    - Added checks to ensure form restoration input exists before accessing its value.
    - Modified the attach function to accept refs, improving the integration with the map picker component.

--- resources/views/infolists/osm-map-entry.blade.php
+++ resources/views/infolists/osm-map-entry.blade.php
@@ -1,15 +1,14 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
 
-    <div x-data="mapPicker($wire, {{ $getMapConfig() }}, @js($getState()))"
-        x-init="async () => {
-            do {
-                await (new Promise(resolve => setTimeout(resolve, 100)));
-        } while (!$refs.map);
-        attach($refs.map);
+    <div x-data="mapPicker($wire, {{ $getMapConfig() }}, @js($getState()))" x-init="async () => {
+        do {
+            await (new Promise(resolve => setTimeout(resolve, 100)));
+        } while (!$refs.map && !$refs.formRestorationInput);
+        attach($refs.map, $refs);
     }" wire:ignore>
         <div x-ref="map" class="w-full" style="min-height: 30vh; {{ $getExtraStyle() }}"></div>
 
-        <input type="text" id="{{ $getId() }}_fmrest" style="display:none"/>
+        <input type="text" x-ref="formRestorationInput" id="{{ $getId() }}_fmrest" style="display:none" />
     </div>
 
 </x-dynamic-component>
